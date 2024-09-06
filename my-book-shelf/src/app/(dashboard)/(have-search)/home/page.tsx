40780a28ad623f0ead7bb99aa611bf67cad8ee13
Hoang Van Nguyen <hoang.nguyenvan@asnet.com.vn>
2024-09-06 15:20:58 +0700

    Feature / Implement isLoading

---
+++ my-book-shelf/src/app/(dashboard)/(have-search)/home/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Grid } from "@chakra-ui/react";
+import { BookType, User } from "@app/models";
+import { Cart } from "@app/components/common";
+import { getAllBook, getUserById, updateUserById } from "@app/api";
+import { useSession } from "next-auth/react";
+import { getTwelveItemData } from "@app/utils";
+import { useRouter } from "next/navigation";
+
+const HomePage = async () => {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  const dataAllBook = await getAllBook();
+  const dataUserById = (await getUserById(session?.user?.id || "")) as User;
+  const dataBook = getTwelveItemData(dataAllBook);
+
+  const handleUpdateFavorites = async (id: string) => {
+    let listFavorite = dataUserById.favorites;
+    if (dataUserById.favorites.includes(id)) {
+      listFavorite = dataUserById.favorites.filter((item) => item !== id);
+    } else {
+      listFavorite = [...dataUserById.favorites, id];
+    }
+
+    await updateUserById(dataUserById.id, {
+      ...dataUserById,
+      favorites: listFavorite,
+    });
+
+    return router.refresh();
+  };
+
+  return (
+    <Grid p="70px 44px" gridTemplateColumns="repeat(6, 1fr)" gap="40px 10px">
+      {dataBook.map((item: BookType) => {
+        const { id, title, author, imageUrl, publicationYear, rating } = item;
+
+        return (
+          <Cart
+            key={id}
+            id={id}
+            title={title}
+            author={author}
+            imageUrl={imageUrl}
+            publicationYear={publicationYear}
+            rating={rating}
+            isFavorite={dataUserById.favorites.includes(id) || false}
+            onUpdateFavorites={handleUpdateFavorites}
+          />
+        );
+      })}
+    </Grid>
+  );
+};
+
+export default HomePage;
